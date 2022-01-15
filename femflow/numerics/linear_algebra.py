adding debugging statements, fixed some numerical issues

femflow/numerics/linear_algebra.py
@@ -2,7 +2,7 @@ from collections import namedtuple
 from typing import Any, List, Tuple, Union
 
 import numpy as np
-from scipy.linalg import expm, polar
+from scipy.linalg import expm
 from scipy.sparse.csr import csr_matrix
 from scipy.spatial.transform import Rotation as R
 
@@ -76,6 +76,17 @@ def fast_diagonal_inverse(mat: csr_matrix):
         mat[i, i] = 1 / mat[i, i]
 
 
+def polar_decomp(m: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+    x = m[0, 0] + m[1, 1]
+    y = m[1, 0] - m[0, 1]
+    scale = 1.0 / np.sqrt(x * x + y * y)
+    c = x * scale
+    s = y * scale
+    r = np.array([[c, -s], [s, c]])
+    s = r.T * m
+    return r, s
+
+
 SVD = namedtuple("SVD", ["u", "sigma", "v"])
 
 
@@ -93,12 +104,12 @@ def svd_2d(m: np.ndarray) -> SVD:
     """
     sig = np.zeros((2, 2))
     v = np.zeros((2, 2))
-    s, u = polar(m)
+    u, s = polar_decomp(m)
 
     c = 0
     s_ = 0
 
-    if abs(s[0, 1]) < 1e-6:
+    if abs(s[0, 1]) < 1e-5:
         sig = s.copy()
         c = 1
         s_ = 0

femflow/simulation/mpm_simulation.py
@@ -56,7 +56,7 @@ class MPMSimulation(object):
         E: float = 1e4,
         v: float = 0.2,
         gravity: float = -200.0,
-        dt: float = 0.0001,
+        dt: float = 1e-4,
         grid_resolution: int = 80,
     ):
         mu, lambda_ = Ev_to_lame_coefficients(E, v)
@@ -73,6 +73,7 @@ class MPMSimulation(object):
             1 / grid_resolution,
             grid_resolution,
             2,
+            False,
         )
 
         self.grid = np.array([])
@@ -82,11 +83,18 @@ class MPMSimulation(object):
         logger.success("Simulation initialized successfully")
 
     def simulate(self, n_timesteps: int):
+        total_steps = 0
         logger.info("Firing up simulation")
         gui = ti.GUI()
         while gui.running and not gui.get_event(gui.ESCAPE):
             for _ in tqdm(range(n_timesteps)):
+                total_steps += 1
+                # if total_steps >= 620:
+                #     self.params.debug = True
+                # if self.params.debug:
+                #     logger.debug(f"TIMESTEP: {total_steps}")
                 self._advance()
+
             gui.clear(0x112F41)
             gui.rect(
                 np.array((0.04, 0.04)), np.array((0.96, 0.96)), radius=2, color=0x4FB99F
@@ -106,7 +114,8 @@ class MPMSimulation(object):
     def _initialize(self):
         # Grid Layout is nxnx3 where each entry is [velocity_x, velocity_y, mass]
         self.grid = np.zeros(
-            (self.params.grid_resolution + 1, self.params.grid_resolution + 1, 3)
+            (self.params.grid_resolution + 1, self.params.grid_resolution + 1, 3),
+            dtype=np.float64,
         )
 
         self._add_object(np.array((0.55, 0.45)), 0xED553B)

femflow/solvers/mpm/grid_to_particle.py
@@ -2,6 +2,7 @@ import functools
 from typing import List
 
 import numpy as np
+from loguru import logger
 
 from femflow.numerics.linear_algebra import svd_2d
 
@@ -16,6 +17,11 @@ def grid_to_particle(
     nvec = functools.partial(np.full, params.dimensions)
 
     for p in particles:
+        if params.debug:
+            logger.warning(f"Before: {p}")
+            logger.warning(
+                f"Grid Stats Before: {grid.max()} {len(grid.nonzero())} {grid.min()}"
+            )
         cell_index = (p.position * params.grid_resolution - nvec(0.5)).astype(np.int64)
 
         # fx
@@ -41,16 +47,16 @@ def grid_to_particle(
                 # w_i_p The weight for the grid cell of this particle
                 weight = weights[i][0] * weights[j][1]
 
+                wxgv = weight * grid_velocity
+
                 # Update the velocity term, v_p
                 # Eqn 175. v_p = sum(w_i_p * v_i)
-                p.velocity += weight * grid_velocity
+                p.velocity += wxgv
 
                 # Update the affine motion term via B_p
-                # Eqn 176. B_p = sum(w_i_p * v_i * (x_i - x_p)^T
+                # Eqn 176. B_p = sum(w_i_p * v_i * (x_i - x_p)^T)
                 inv_dx = params.grid_resolution
-                p.affine_momentum += (
-                    4 * inv_dx * np.outer(np.dot(weight, grid_velocity), dpos)
-                )
+                p.affine_momentum += 4 * inv_dx * np.outer(wxgv, dpos)
 
         # Advection step
         p.position += params.dt * p.velocity
@@ -75,3 +81,8 @@ def grid_to_particle(
         p.volume = J_p_new
         p.deformation_gradient = F
 
+        if params.debug:
+            logger.warning(f"After: {p}")
+            logger.warning(
+                f"Grid Stats After: {grid.max()} {len(grid.nonzero())} {grid.min()}"
+            )

femflow/solvers/mpm/particle_to_grid.py
@@ -3,7 +3,8 @@ from typing import List
 
 import numpy as np
 from loguru import logger
-from scipy.linalg import polar
+
+from femflow.numerics.linear_algebra import polar_decomp
 
 from .parameters import MPMParameters
 from .particle import NeoHookeanParticle
@@ -56,7 +57,7 @@ def fixed_corotated_stress(params: MPMParameters, particle: NeoHookeanParticle):
     current_volume = np.linalg.det(particle.deformation_gradient)
 
     # F = r, s; Rotation Matrix and Symmetric Matrix
-    r, _ = polar(particle.deformation_gradient)
+    r, _ = polar_decomp(particle.deformation_gradient)
 
     # Cauchy stress
     PF = np.matmul(
@@ -89,6 +90,11 @@ def particle_to_grid(
     grid[:, :, :] = 0
 
     for p in particles:
+        if params.debug:
+            logger.debug(f"Before: {p}")
+            logger.debug(
+                f"Grid Stats Before: {grid.max()} {len(grid.nonzero())} {grid.min()}"
+            )
         cell_index = (p.position * params.grid_resolution - nvec(0.5)).astype(np.int64)
 
         # fx
@@ -113,9 +119,14 @@ def particle_to_grid(
                     weight = weights[i][0] * weights[j][1]
 
                     dpos = (np.array((i, j)) - cell_difference) * params.dx
+                    vxy = p.velocity * p.mass
+
+                    assert len(vxy) == params.dimensions
 
                     # Compute the translational momentum of the particle
-                    mv = np.array((*(p.velocity * p.mass), p.mass))
+                    mv = np.array((vxy[0], vxy[1], p.mass))
+
+                    affinexdx = np.dot(affine, dpos)
 
                     # Compute the density for this particle in relation to the others
                     grid[cell_index[0] + i, cell_index[1] + j] += weight * (
@@ -126,3 +137,8 @@ def particle_to_grid(
             logger.debug(f"Cell index: {cell_index}")
             logger.debug(f"Particle: {p}")
             exit(1)
+        if params.debug:
+            logger.debug(f"After: {p}")
+            logger.debug(
+                f"Grid Stats After: {grid.max()} {len(grid.nonzero())} {grid.min()}"
+            )